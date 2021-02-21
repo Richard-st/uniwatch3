278aeb3fec08aaaead45e26a023d9cb68af069e0
Richard <richard.stanners@gmail.com>
2021-02-21 13:27:12 +1300

    watchdog debounce

--- unnitest/app/watchWatchdog.py
+++ unnitest/app/watchWatchdog.py
@@ -2,6 +2,9 @@
 from watchdog.observers import Observer
 from watchdog.events import PatternMatchingEventHandler
 from app import sockets
+import time
+
+lastCreateTimeEpochMS = 0
 
 def notify():
     print ("notify started")
@@ -20,13 +23,23 @@ def notify():
     #------------------------
     event_handler = PatternMatchingEventHandler(patterns, ignore_patterns, ignore_directories, case_sensitive)
 
-    event_handler.on_created = on_file_change
+    event_handler.on_created = on_file_created
 
     observer = Observer()
     observer.schedule(event_handler, path, recursive=go_recursively)
     observer.start()
 
-def on_file_change(event):
+def on_file_created(event):
+    global lastCreateTimeEpochMS
+    #debounce - not sure why I get 2 creates, so wait 0.1 secs
 
-    print (str( {event.src_path} ) + "file created")
-    sockets.sendMessageToAllCllients()
+    currentTimeEpochMS  = round(time.time() * 1000)
+
+    if (currentTimeEpochMS > lastCreateTimeEpochMS + 100):
+        print (str( {event.src_path} ) + "file created")
+        print (str(event))
+        sockets.sendMessageToAllCllients() 
+    else:      
+        lastCreateTimeEpochMS = round(time.time() * 1000)
+
+
